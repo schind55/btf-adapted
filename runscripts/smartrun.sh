ab5576fc3df9da55859d6db0b06f228484697877
schind55 <schind55@ads.uni-passau.de>
2021-10-23 04:28:57 -0700

    Lauffähige Version

--- runscripts/smartrun.sh
+++ runscripts/smartrun.sh
@@ -12,4 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# shellcheck disable=SC2068
 java -Djava.security.properties="./config/java.security" -Dlogback.configurationFile="./config/logback.xml" -cp bin/*:lib/* $@
